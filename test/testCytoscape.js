eefa45b61e4df5eff72051c2959bc968299b1652
Unknown <sandra.bochnakian@gmail.com>
2018-04-06 18:21:20 +0200

    New repo Projet-TalentsThree

---
+++ test/testCytoscape.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+  class TalentTree {
+
+    getData() {
+      return fetch("CytoData.json")
+        .then(response => response.json())
+        .then(data => {
+          return this.getCytoscape(data.skills);
+        });
+    }
+
+    getCytoscape(data) {
+      var i=0;
+      console.log(data[i].title);
+      data[i];
+      console.log(data);
+      var cy = cytoscape({
+        container: document.getElementById("cy")
+      });
+      data.forEach(function(data,i) {
+        var name = data.title;
+        console.log(name, i);
+    
+        elements: [
+            { data: { id: name} },
+            {
+            data: {
+                id: name,
+                source: name,
+                target: name
+              }
+            }
+            ]
+            // for (var i = 0; i < data.lenght; i++) {
+            //     cy.add({
+            //         data: { id: 'node' + i }
+            //         }
+            //     );
+            // }
+        })
+      style: [
+        {
+          selector: "node",
+          style: {
+            shape: "hexagon",
+            "background-color": "red",
+            label: 'name'
+          }
+        }
+      ];
+      layout: {
+        name: "grid";
+      }
+      cy
+        .layout({
+          name: "circle"
+        })
+        .run();
+    }
+}
+  var talents = new TalentTree();  
+  talents.getData();
+
+//  talents.getCytoscape();
+  console.log(talents);
+})
