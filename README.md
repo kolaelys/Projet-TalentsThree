5f607f6a7ec4b2e574a0afbcbc6c7dec4162381c
Unknown <sandra.bochnakian@gmail.com>
2018-04-09 12:46:40 +0200

    Mise à jour du README

--- README.md
+++ README.md
@@ -9,8 +9,8 @@
 > je travaille actuellement sur les fichiers:
 * test/
   * testCytoscape.html
-  * testCytoscape.js
-  * CytoData.json
+  * glycolysis.js
+  * CytoData.js
  
 
  #### Sources
@@ -20,25 +20,25 @@
   
  #### Ce que j'ai fais
  * Dans testCytoscape.html:
-   * J'ai simplement crée une div pour contenir le graph Cytoscape
-* Dans CytoData.json:
-  * J'ai copié le fichier `tft.dnd.data.js` de l'arbre-web de Xavier
-* Dans testCyscape.js:
-  * J'ai créer une Class **TalentTree**
-  * J'ai créer une fonction *getData* qui retourne les compétences du fichiers CytoData.json
-  * J'ai une fonction getCytoscape qui, pour le moment, récupère les titres des compétences dans l'espoir de les afficher sur le graph
+   * J'ai simplement crée une div pour contenir le graph Cytoscap
+
+* Dans CytoData.js:
+  * J'ai copié le fichier `tft.dnd.data.js` de l'arbre-web de Xavier 
+  * j'y ai rajouté les liens entre les compétences grâce à l'élément "edge" de la librairie
+
+* Dans glycolysis.js:
+  * J'ai créer le graph grâce à la variable "cy" qui contiendra les informations du fichier data pour les afficher. 
+
 
  
 #### Ce que j'essaie de faire 
-> J'aimerais réussir à afficher les titres des compétences dans le graph qui utilise la librairie `Cytoscape.js` 
-J'arrive à obtenir les 26 points des 26 datas mais pas à faire apparaître les données que je récupère...
+> Réorganiser l'apparence du grapique pour obtenir une vu agréable et intuitive
 
 
 
 #### Ce que je prévois de faire après
 
 * Trouver le moyen de réutiliser les fonctions de Xavier avec Cytoscape.js
-> Après avoir étudier les fonctions du fichier `tft.dnd.data.js`, je pense qu'il est possible de les modifier un peu de façon à les inclures dans le nouveau code, tout en en gardant le principal grâce au système de dépendance du fichier contenant les datas. Cependant, il est tout à fait possible que je sous-estime la difficulté de cette transition...
 
 
 
--- test/testCytoscape.js
+++ test/testCytoscape.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+
   class TalentTree {
 
     getData() {
@@ -14,50 +16,51 @@ $(document).ready(function() {
       console.log(data[i].title);
       data[i];
       console.log(data);
-      var cy = cytoscape({
-        container: document.getElementById("cy")
-      });
-      data.forEach(function(data,i) {
-        var name = data.title;
-        console.log(name, i);
+//       var cy = cytoscape({
+//         container: document.getElementById("cy")
+//       });
+//       data.forEach(function(data,i) {
+//         var name = data.title;
+//         var depends = data.dependsOn;
+//         console.log(name, i, depends);
     
-        elements: [
-            { data: { id: name} },
-            {
-            data: {
-                id: name,
-                source: name,
-                target: name
-              }
-            }
-            ]
-            // for (var i = 0; i < data.lenght; i++) {
-            //     cy.add({
-            //         data: { id: 'node' + i }
-            //         }
-            //     );
-            // }
-        })
-      style: [
-        {
-          selector: "node",
-          style: {
-            shape: "hexagon",
-            "background-color": "red",
-            label: 'name'
-          }
-        }
-      ];
-      layout: {
-        name: "grid";
-      }
-      cy
-        .layout({
-          name: "circle"
-        })
-        .run();
-    }
-}
+//         elements: [
+//             { data: { id: name} },
+//             {
+//             data: {
+//                 id: name,
+//                 source: name,
+//                 target: name 
+//               }
+//             }
+//             ]
+//             // for (var i = 0; i < data.lenght; i++) {
+//             //     cy.add({
+//             //         data: { id: 'node' + i }
+//             //         }
+//             //     );
+//             // }
+//         })
+//       style: [
+//         {
+//           selector: "node",
+//           style: {
+//             shape: "hexagon",
+//             "background-color": "red",
+//             label: 'name'
+//           }
+//         }
+//       ];
+//       layout: {
+//         name: "grid";
+//       }
+//       cy
+//         .layout({
+//           name: "circle"
+//         })
+//         .run();
+     }
+ }
   var talents = new TalentTree();  
   talents.getData();
 
